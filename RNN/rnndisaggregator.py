a76e29eff684eaa541a05e894af195731205a1ba
Rotem Mordoch <mordochrotem@gmail.com>
2017-12-19 17:33:23 +0100

    evaluate training across buildings

--- RNN/rnndisaggregator.py
+++ RNN/rnndisaggregator.py
@@ -121,59 +121,82 @@ class RNNDisaggregator(Disaggregator):
         loss = self.model.evaluate(val_mainchunk, val_meterchunk, batch_size=batch_size)
         self.update_logfile(self.val_logfile, [loss], self.total_epochs-1)
 
-    def train_across_buildings(self, mainlist, meterlist, epochs=1, batch_size=128, **load_kwargs):
-        '''Train using data from multiple buildings
+    def train_across_buildings(self, train_mainlist, train_meterlist, val_mainlist, val_meterlist, epochs=1,
+                               batch_size=128, **load_kwargs):
+        '''Train using data from multiple train_buildings
 
         Parameters
         ----------
-        mainlist : a list of nilmtk.ElecMeter objects for the aggregate data of each building
-        meterlist : a list of nilmtk.ElecMeter objects for the meter data of each building
+        train_mainlist : a list of nilmtk.ElecMeter objects for the aggregate data of each building
+        train_meterlist : a list of nilmtk.ElecMeter objects for the meter data of each building
         batch_size : size of batch used for training
         epochs : number of epochs to train
         **load_kwargs : keyword arguments passed to `meter.power_series()`
         '''
 
-        assert(len(mainlist) == len(meterlist), "Number of main and meter channels should be equal")
-        num_meters = len(mainlist)
+        assert(len(train_mainlist) == len(train_meterlist), "Number of train main and meter channels should be equal")
+        assert(len(val_mainlist) == len(val_meterlist), "Number of validation main and meter channels should be equal")
+        train_num_meters = len(train_mainlist)
+        val_num_meters = len(val_mainlist)
 
-        mainps = [None] * num_meters
-        meterps = [None] * num_meters
-        mainchunks = [None] * num_meters
-        meterchunks = [None] * num_meters
+        train_mainps = [None] * train_num_meters
+        train_meterps = [None] * train_num_meters
+        train_mainchunks = [None] * train_num_meters
+        train_meterchunks = [None] * train_num_meters
+        val_mainps = [None] * val_num_meters
+        val_meterps = [None] * val_num_meters
+        val_mainchunks = [None] * val_num_meters
+        val_meterchunks = [None] * val_num_meters
 
         # Get generators of timeseries
-        for i,m in enumerate(mainlist):
-            mainps[i] = m.power_series(**load_kwargs)
+        for i,m in enumerate(train_mainlist):
+            train_mainps[i] = m.power_series(**load_kwargs)
 
-        for i,m in enumerate(meterlist):
-            meterps[i] = m.power_series(**load_kwargs)
+        for i,m in enumerate(train_meterlist):
+            train_meterps[i] = m.power_series(**load_kwargs)
+
+        for i,m in enumerate(val_mainlist):
+            val_mainps[i] = m.power_series(**load_kwargs)
+
+        for i,m in enumerate(val_meterlist):
+            val_meterps[i] = m.power_series(**load_kwargs)
 
         # Get a chunk of data
-        for i in range(num_meters):
-            mainchunks[i] = next(mainps[i])
-            meterchunks[i] = next(meterps[i])
+        for i in range(train_num_meters):
+            train_mainchunks[i] = next(train_mainps[i])
+            train_meterchunks[i] = next(train_meterps[i])
+        for i in range(val_num_meters):
+            val_mainchunks[i] = next(val_mainps[i])
+            val_meterchunks[i] = next(val_meterps[i])
         if self.mmax is None:
-            self.mmax = max([m.max() for m in mainchunks])
+            self.mmax = max([m.max() for m in train_mainchunks])
 
         run = True
         while run:
             # Normalize and train
-            mainchunks = [self._normalize(m, self.mmax) for m in mainchunks]
-            meterchunks = [self._normalize(m, self.mmax) for m in meterchunks]
+            train_mainchunks = [self._normalize(m, self.mmax) for m in train_mainchunks]
+            train_meterchunks = [self._normalize(m, self.mmax) for m in train_meterchunks]
+            val_mainchunks = [self._normalize(m, self.mmax) for m in val_mainchunks]
+            val_meterchunks = [self._normalize(m, self.mmax) for m in val_meterchunks]
 
-            self.train_across_buildings_chunk(mainchunks, meterchunks, epochs, batch_size)
+            self.train_across_buildings_chunk(train_mainchunks, train_meterchunks, val_mainchunks, val_meterchunks,
+                                              epochs, batch_size)
 
             # If more chunks, repeat
             try:
-                for i in range(num_meters):
-                    mainchunks[i] = next(mainps[i])
-                    meterchunks[i] = next(meterps[i])
+                for i in range(train_num_meters):
+                    train_mainchunks[i] = next(train_mainps[i])
+                    train_meterchunks[i] = next(train_meterps[i])
+                for i in range(val_num_meters):
+                    val_mainchunks[i] = next(val_mainps[i])
+                    val_meterchunks[i] = next(val_meterps[i])
             except:
                 run = False
 
-    def train_across_buildings_chunk(self, mainchunks, meterchunks, epochs, batch_size):
+    def train_across_buildings_chunk(self, train_mainchunks, train_meterchunks, val_mainchunks, val_meterchunks, epochs,
+                                     batch_size):
         '''Train using only one chunk of data. This chunk consists of data from
-        all buildings.
+        all train_buildings.
 
         Parameters
         ----------
@@ -182,56 +205,104 @@ class RNNDisaggregator(Disaggregator):
         epochs : number of epochs for training
         batch_size : size of batch used for training
         '''
-        num_meters = len(mainchunks)
-        batch_size = int(batch_size/num_meters)
-        num_of_batches = [None] * num_meters
+        train_num_meters = len(train_mainchunks)
+        train_batch_size = int(batch_size / train_num_meters)
+        train_num_of_batches = [None] * train_num_meters
+
+        val_num_meters = len(val_mainchunks)
+        val_batch_size = int(batch_size / val_num_meters)
+        val_num_of_batches = [None] * val_num_meters
 
         # Find common parts of timeseries
-        for i in range(num_meters):
-            mainchunks[i].fillna(0, inplace=True)
-            meterchunks[i].fillna(0, inplace=True)
-            ix = mainchunks[i].index.intersection(meterchunks[i].index)
-            m1 = mainchunks[i]
-            m2 = meterchunks[i]
-            mainchunks[i] = m1[ix]
-            meterchunks[i] = m2[ix]
+        for i in range(train_num_meters):
+            train_mainchunks[i].fillna(0, inplace=True)
+            train_meterchunks[i].fillna(0, inplace=True)
+            ix = train_mainchunks[i].index.intersection(train_meterchunks[i].index)
+            m1 = train_mainchunks[i]
+            m2 = train_meterchunks[i]
+            train_mainchunks[i] = m1[ix]
+            train_meterchunks[i] = m2[ix]
 
-            num_of_batches[i] = int(len(ix)/batch_size) - 1
+            train_num_of_batches[i] = int(len(ix) / train_batch_size) - 1
+
+        for i in range(val_num_meters):
+            val_mainchunks[i].fillna(0, inplace=True)
+            val_meterchunks[i].fillna(0, inplace=True)
+            ix = val_mainchunks[i].index.intersection(val_meterchunks[i].index)
+            m1 = val_mainchunks[i]
+            m2 = val_meterchunks[i]
+            val_mainchunks[i] = m1[ix]
+            val_meterchunks[i] = m2[ix]
+
+            val_num_of_batches[i] = int(len(ix) / val_batch_size) - 1
 
         for e in range(epochs): # Iterate for every epoch
             print(e)
-            loss = 0
-            batch_indexes = list(range(min(num_of_batches)))
+            train_loss = 0
+            batch_indexes = list(range(min(train_num_of_batches)))
             random.shuffle(batch_indexes)
 
             for bi, b in enumerate(batch_indexes): # Iterate for every batch
-                print("Batch {} of {}".format(bi, min(num_of_batches)), end="\n")
+                print("Batch {} of {}".format(bi, min(train_num_of_batches)), end="\n")
                 sys.stdout.flush()
-                X_batch = np.empty((batch_size*num_meters, 1, 1))
-                Y_batch = np.empty((batch_size*num_meters, 1))
+                X_batch = np.empty((train_batch_size * train_num_meters, 1, 1))
+                Y_batch = np.empty((train_batch_size * train_num_meters, 1))
 
-                # Create a batch out of data from all buildings
-                for i in range(num_meters):
-                    mainpart = mainchunks[i]
-                    meterpart = meterchunks[i]
-                    mainpart = mainpart[b*batch_size:(b+1)*batch_size]
-                    meterpart = meterpart[b*batch_size:(b+1)*batch_size]
-                    X = np.reshape(mainpart, (batch_size, 1, 1))
-                    Y = np.reshape(meterpart, (batch_size, 1))
+                # Create a batch out of data from all train_buildings
+                for i in range(train_num_meters):
+                    mainpart = train_mainchunks[i]
+                    meterpart = train_meterchunks[i]
+                    mainpart = mainpart[b * train_batch_size:(b + 1) * train_batch_size]
+                    meterpart = meterpart[b * train_batch_size:(b + 1) * train_batch_size]
+                    X = np.reshape(mainpart, (train_batch_size, 1, 1))
+                    Y = np.reshape(meterpart, (train_batch_size, 1))
 
-                    X_batch[i*batch_size:(i+1)*batch_size] = np.array(X)
-                    Y_batch[i*batch_size:(i+1)*batch_size] = np.array(Y)
+                    X_batch[i * train_batch_size:(i + 1) * train_batch_size] = np.array(X)
+                    Y_batch[i * train_batch_size:(i + 1) * train_batch_size] = np.array(Y)
 
                 # Shuffle data
                 p = np.random.permutation(len(X_batch))
                 X_batch, Y_batch = X_batch[p], Y_batch[p]
 
                 # Train model
-                loss += self.model.train_on_batch(X_batch, Y_batch)
-            loss /= min(num_of_batches)
+                train_loss += self.model.train_on_batch(X_batch, Y_batch)
+            train_loss /= min(train_num_of_batches)
             print("\n")
-            with open(self.train_logfile, "a") as f:
-                f.write("{},{}\n".format(e, loss))
+            self.update_logfile(self.train_logfile, [train_loss], e+self.total_epochs)
+
+            val_loss = 0
+            batch_indexes = list(range(min(val_num_of_batches)))
+            random.shuffle(batch_indexes)
+
+            for bi, b in enumerate(batch_indexes):  # Iterate for every batch
+                print("Batch {} of {}".format(bi, min(val_num_of_batches)), end="\n")
+                sys.stdout.flush()
+                X_batch = np.empty((val_batch_size * val_num_meters, 1, 1))
+                Y_batch = np.empty((val_batch_size * val_num_meters, 1))
+
+                # Create a batch out of data from all train_buildings
+                for i in range(val_num_meters):
+                    mainpart = val_mainchunks[i]
+                    meterpart = val_meterchunks[i]
+                    mainpart = mainpart[b * val_batch_size:(b + 1) * val_batch_size]
+                    meterpart = meterpart[b * val_batch_size:(b + 1) * val_batch_size]
+                    X = np.reshape(mainpart, (val_batch_size, 1, 1))
+                    Y = np.reshape(meterpart, (val_batch_size, 1))
+
+                    X_batch[i * val_batch_size:(i + 1) * val_batch_size] = np.array(X)
+                    Y_batch[i * val_batch_size:(i + 1) * val_batch_size] = np.array(Y)
+
+                # Shuffle data
+                p = np.random.permutation(len(X_batch))
+                X_batch, Y_batch = X_batch[p], Y_batch[p]
+
+                # Evaluate model
+                val_loss += self.model.test_on_batch(X_batch, Y_batch)
+            val_loss /= min(val_num_of_batches)
+            print("\n")
+            self.update_logfile(self.val_logfile, [val_loss], e+self.total_epochs)
+
+        self.total_epochs += epochs
 
     def disaggregate(self, mains, output_datastore, results_file, meter_metadata, **load_kwargs):
         '''Disaggregate mains according to the model learnt previously.
--- RNN/simulator-across-buildings-test.py
+++ RNN/simulator-across-buildings-test.py
@@ -14,27 +14,39 @@ from rnndisaggregator import RNNDisaggregator
 print("========== OPEN DATASETS ============")
 # from nilmtk.dataset_converters import convert_redd
 # convert_simulator('../data/SIMULATOR/low_freq', '../data/SIMULATOR/simulator.csv', format='CSV')
-print("========== OPEN DATASETS ============")
 train = DataSet('../data/SIMULATOR/simulator.csv', format='CSV')
 train.clear_cache()
+validation = DataSet('../data/SIMULATOR/simulator.csv', format='CSV')
+validation.clear_cache()
 test = DataSet('../data/SIMULATOR/simulator.csv', format='CSV')
 test.clear_cache()
 
-mainslist = []
-meterlist = []
-buildings = 4
+train_mainslist = []
+train_meterlist = []
+val_mainslist = []
+val_meterlist = []
+train_buildings = 3
+val_buildings = 2
 sample_period = 1
 meter_key = 'dish washer'
-for i in range(1,buildings):
+for i in range(1, train_buildings+1):
     train_elec = train.buildings[i].elec
     train_meter = train_elec.submeters()[meter_key]
     train_mains_instance = train_meter.upstream_meter().instance()
     train_mains = train_elec.mains().all_meters()[train_mains_instance-1]
 
-    mainslist += [train_mains]
-    meterlist += [train_meter]
+    train_mainslist += [train_mains]
+    train_meterlist += [train_meter]
+for i in range(train_buildings+1, val_buildings+train_buildings+1):
+    val_elec = validation.buildings[i].elec
+    val_meter = val_elec.submeters()[meter_key]
+    val_mains_instance = val_meter.upstream_meter().instance()
+    val_mains = val_elec.mains().all_meters()[val_mains_instance-1]
 
-test_elec = test.buildings[buildings].elec
+    val_mainslist += [val_mains]
+    val_meterlist += [val_meter]
+
+test_elec = test.buildings[val_buildings+train_buildings+1].elec
 test_meter = test_elec.submeters()[meter_key]
 test_mains_instance = test_meter.upstream_meter().instance()
 test_mains = test_elec.mains().all_meters()[test_mains_instance-1]
@@ -50,9 +62,10 @@ start = time.time()
 print("========== TRAIN ============")
 epochs = 0
 for i in range(1):
-    rnn.train_across_buildings(mainslist, meterlist, epochs=1, sample_period=sample_period)
+    rnn.train_across_buildings(train_mainslist, train_meterlist, val_mainslist, val_meterlist, epochs=1,
+                               sample_period=sample_period)
     epochs += 1
-    rnn.export_model(os.path.join(results_dir, "SIMULATOR-RNN-h{}-{}-{}-{}epochs.h5".format(1, buildings, meter_key, epochs)))
+    rnn.export_model(os.path.join(results_dir, "SIMULATOR-RNN-h{}-{}-{}-{}epochs.h5".format(1, train_buildings, meter_key, epochs)))
     print("CHECKPOINT {}".format(epochs))
 end = time.time()
 print("Train =", end-start, "seconds.")
@@ -64,7 +77,7 @@ with open(results_file, "w") as text_file:
 print(headline)
 disag_filename = 'disag-out.h5'
 output = HDFDataStore(os.path.join(results_dir, disag_filename), 'w')
-rnn.disaggregate(test_mains, output, results_file, meterlist[0], sample_period=sample_period)
+rnn.disaggregate(test_mains, output, results_file, train_meterlist[0], sample_period=sample_period)
 output.close()
 
 
@@ -73,7 +86,7 @@ with open(results_file, "a") as text_file:
     text_file.write(headline + '\n')
 print(headline)
 result = DataSet(os.path.join(results_dir, disag_filename))
-res_elec = result.buildings[buildings].elec
+res_elec = result.buildings[val_buildings+train_buildings+1].elec
 rpaf = metrics.recall_precision_accuracy_f1(res_elec[meter_key], test_elec[meter_key])
 
 lines = ["============ Recall: {}".format(rpaf[0]),
