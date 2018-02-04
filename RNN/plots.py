2a64484573a12106c445ec6324be1127a180c088
Rotem Mordoch <mordochrotem@gmail.com>
2018-02-04 17:44:28 +0100

    use ribbon plots in plot_prediction_over_epochs, allow different windows for different houses for train set in plot_datasets_meter

--- RNN/plots.py
+++ RNN/plots.py
@@ -14,16 +14,16 @@ import plotly
 def plot_prediction_over_epochs():
     train = DataSet('../data/ukdale.h5')
     train.clear_cache()
-    train.set_window(start="18-4-2013", end="14-5-2013")
+    train.set_window(start="13-4-2013", end="31-7-2013")
     test = DataSet('../data/ukdale.h5')
     test.clear_cache()
-    test.set_window(start="21-5-2013", end="24-5-2013")
+    test.set_window(start="23-7-2014 10:00:00", end="23-7-2014 11:00:00")
 
     train_building = 1
-    test_building = 1
+    test_building = 5
     sample_period = 6
     meter_key = 'kettle'
-    learning_rate = 1e-3
+    learning_rate = 1e-5
 
     train_elec = train.buildings[train_building].elec
     test_elec = test.buildings[test_building].elec
@@ -31,7 +31,7 @@ def plot_prediction_over_epochs():
     train_meter = train_elec.submeters()[meter_key]
     test_mains = test_elec.mains()
 
-    results_dir = '../results/UKDALE-RNN-2018-01-12 00:05:42'
+    results_dir = '../results/UKDALE-ACROSS-BUILDINGS-RNN-lr=1e-05-2018-02-03-11-48-12'
     train_logfile = os.path.join(results_dir, 'training.log')
     val_logfile = os.path.join(results_dir, 'validation.log')
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate, init=False)
@@ -78,13 +78,11 @@ def plot_prediction_over_epochs():
     # ax.set_zlabel('epochs')
     # plt.show()
 
-
-    fill_colors = ['rgba(0,255,0,0.1)', 'rgba(255,0,0,1)']
     data = []
 
-    for i in range(10, 21, 10):
+    for i in range(10, 401, 10):
         # disaggregate model
-        model = 'UKDALE-RNN-h1-kettle-{}epochs.h5'.format(i)
+        model = 'UKDALE-RNN-kettle-{}epochs.h5'.format(i)
         rnn.import_model(os.path.join(results_dir, model))
         disag_filename = 'disag-out-{}epochs.h5'.format(i)
         output = HDFDataStore(os.path.join(results_dir, disag_filename), 'w')
@@ -101,28 +99,40 @@ def plot_prediction_over_epochs():
         predicted = predicted.power_series(sample_period=sample_period)
         predicted = next(predicted)
         predicted.fillna(0, inplace=True)
-        # timestamps = np.array(predicted.keys())
-        # power = np.array(predicted)
-        # timestamps = np.array(range(power.shape[0]))
         power = predicted.tolist()
         length = len(power)
         timestamps = list(range(length))
         epochs = [i] * length
 
+        x = []
+        y = []
+        z = []
+        ci = int(255 / 420 * i)  # ci = "color index"
+        for j in range(length):
+            x.append([timestamps[j], timestamps[j]])
+            y.append([i, i + 5])
+            z.append([power[j], power[j]])
         data.append(dict(
-            type='scatter3d',
-            mode='lines',
-            x=timestamps,
-            y=epochs,
-            z=power,
-            name='',
-            surfaceaxis=1,  # add a surface axis ('1' refers to axes[1] i.e. the y-axis)
-            surfacecolor=fill_colors[0],
-            line=dict(
-                color='black',
-                width=4
-            ),
+            z=z,
+            x=x,
+            y=y,
+            colorscale=[[i, 'rgb(%d,%d,255)' % (ci, ci)] for i in np.arange(0, 1.1, 0.1)],
+            showscale=False,
+            type='surface',
         ))
+
+        # data.append(dict(
+        #     type='scatter3d',
+        #     mode='lines',
+        #     x=timestamps,
+        #     y=epochs,
+        #     z=power,
+        #     name='',
+        #     line=dict(
+        #         color='black',
+        #         width=4
+        #     ),
+        # ))
 
     # plot ground truth curve as the last curve
     ground_truth = test_elec[meter_key]
@@ -133,22 +143,38 @@ def plot_prediction_over_epochs():
     power = ground_truth.tolist()
     length = len(power)
     timestamps = list(range(length))
-    epochs = [30] * length
+    epochs = [410] * length
 
+    i = 410
+    x = []
+    y = []
+    z = []
+    ci = int(255 / 410 * i)  # ci = "color index"
+    for j in range(length):
+        x.append([timestamps[j], timestamps[j]])
+        y.append([i, i + 5])
+        z.append([power[j], power[j]])
     data.append(dict(
-        type='scatter3d',
-        mode='lines',
-        x=timestamps,
-        y=epochs,
-        z=power,
-        name='',
-        surfaceaxis=1,  # add a surface axis ('1' refers to axes[1] i.e. the y-axis)
-        surfacecolor=fill_colors[1],
-        line=dict(
-            color='black',
-            width=4
-        ),
+        z=z,
+        x=x,
+        y=y,
+        colorscale=[[i, 'rgb(%d,%d,255)' % (ci, ci)] for i in np.arange(0, 1.1, 0.1)],
+        showscale=False,
+        type='surface',
     ))
+
+    # data.append(dict(
+    #     type='scatter3d',
+    #     mode='lines',
+    #     x=timestamps,
+    #     y=epochs,
+    #     z=power,
+    #     name='',
+    #     line=dict(
+    #         color='black',
+    #         width=4
+    #     ),
+    # ))
 
     layout = dict(
         title='prediction over epochs',
@@ -164,10 +190,6 @@ def plot_prediction_over_epochs():
     )
 
     fig = dict(data=data, layout=layout)
-
-    # IPython notebook
-    # py.iplot(fig, filename='filled-3d-lines')
-
     plotly.offline.plot(fig, filename='filled-3d-lines')
 
 
@@ -206,7 +228,7 @@ def plot_datasets_meter():
     windows = {
         'train': [['2-1-2014', '15-5-2014'], ['30-3-2013', '15-7-2013']],
         'validation': ['13-4-2013', '13-6-2013'],
-        'test': ['30-6-2014', '30-7-2014']
+        'test': ['23-7-2014 10:40:00', '23-7-2014 11:00:00']
     }
 
     train = []
@@ -227,7 +249,7 @@ def plot_datasets_meter():
     val_buildings = [4]
     test_building = 5
     sample_period = 6
-    meter_key = 'fridge'
+    meter_key = 'kettle'
 
     train_meterlist = []
     val_meterlist = []
@@ -242,6 +264,8 @@ def plot_datasets_meter():
 
     test_elec = test.buildings[test_building].elec
     test_meter = test_elec.submeters()[meter_key]
+    test_meter.plot()
+    plt.show()
 
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=False, sharey=True)
     fig.set_size_inches(15.5, 10.5)
@@ -255,4 +279,4 @@ def plot_datasets_meter():
 
 
 if __name__ == "__main__":
-    plot_datasets_meter()
+    plot_prediction_over_epochs()
