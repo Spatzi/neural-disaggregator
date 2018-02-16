cb0be40c7003b9d3ac3a88b2e3df016a693a4920
Rotem Mordoch <mordochrotem@gmail.com>
2018-02-16 23:10:07 +0100

    import model

--- RNN/ukdale-test.py
+++ RNN/ukdale-test.py
@@ -75,14 +75,14 @@ val_logfile = os.path.join(results_dir, 'validation.log')
 
 if IMPORT:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate, init=False)
-    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-60epochs.h5"))  # TODO: insert last model name
+    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-100epochs.h5"))  # TODO: insert last model name
 else:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate)
 
 print("========== TRAIN ============")
-epochs = 60  # TODO: update according to the last model if IMPORT = True
+epochs = 100  # TODO: update according to the last model if IMPORT = True
 start = time.time()
-for i in range(4):
+for i in range(5):
     rnn.train(train_mains, train_meter, validation_mains, validation_meter, epochs=10, sample_period=sample_period)
     epochs += 10
     rnn.export_model(os.path.join(results_dir, "UKDALE-RNN-{}-{}epochs.h5".format(meter_key, epochs)))
