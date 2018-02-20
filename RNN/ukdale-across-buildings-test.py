22cee54de73d4d75c7067072464da36d48ef6284
Rotem Mordoch <mordochrotem@gmail.com>
2018-02-20 12:26:22 +0100

    change test set

--- RNN/ukdale-across-buildings-test.py
+++ RNN/ukdale-across-buildings-test.py
@@ -17,7 +17,7 @@ IMPORT = True  # TODO: True if continue training
 windows = {
     'train': [['13-4-2013', '13-6-2013'], ['13-6-2013', '31-7-2013']],
     'validation': ['13-5-2013', '13-6-2013'],
-    'test': ['15-7-2014', '30-7-2014']
+    'test': ['30-6-2014', '15-7-2014']
 }
 
 # windows = {
@@ -98,14 +98,14 @@ val_logfile = os.path.join(results_dir, 'validation.log')
 
 if IMPORT:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate, init=False)
-    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-100epochs.h5"))  # TODO: insert last model name
+    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-150epochs.h5"))  # TODO: insert last model name
 else:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate)
 
 start = time.time()
 print("========== TRAIN ============")
-epochs = 100  # TODO: update according to the last model if IMPORT = True
-for i in range(5):
+epochs = 150  # TODO: update according to the last model if IMPORT = True
+for i in range(0):
     rnn.train_across_buildings(train_mainslist, train_meterlist, val_mainslist, val_meterlist, epochs=10,
                                sample_period=sample_period)
     epochs += 10
