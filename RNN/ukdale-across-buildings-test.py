cd2f1180bab7d5b55e2d0c650d678038b2dd7ba5
Rotem Mordoch <mordochrotem@gmail.com>
2018-02-20 12:19:09 +0100

    evaluate validation set

--- RNN/ukdale-across-buildings-test.py
+++ RNN/ukdale-across-buildings-test.py
@@ -17,7 +17,7 @@ IMPORT = True  # TODO: True if continue training
 windows = {
         'train': [["13-4-2013", "6-7-2013"], ["13-4-2013", "6-7-2013"]],
         'validation': ["6-7-2013", "21-6-2013"],
-        'test': ["24-7-2014", "31-7-2014"]
+        'test': ["6-7-2013", "21-6-2013"]
     }
 
 # windows = {
