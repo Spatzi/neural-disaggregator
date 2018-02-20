1be00d0c7b756902d0300eb39fde7450e5727ba3
Rotem Mordoch <mordochrotem@gmail.com>
2018-02-20 15:24:30 +0100

    train on more buildings

--- RNN/ukdale-across-buildings-test.py
+++ RNN/ukdale-across-buildings-test.py
@@ -12,12 +12,14 @@ from rnndisaggregator import RNNDisaggregator
 from plots import plot_loss
 
 
-IMPORT = True  # TODO: True if continue training
+IMPORT = False  # TODO: True if continue training
 
 windows = {
-    'train': [['13-4-2013', '13-6-2013'], ['13-6-2013', '31-7-2013']],
-    'validation': ['13-5-2013', '13-6-2013'],
-    'test': ['13-6-2013', '30-6-2013']
+    'train': [['13-4-2013', '13-5-2013'], ['13-6-2013', '13-7-2013'], ['13-5-2013', '13-6-2013'],
+              ['13-7-2014', '31-7-2014']],
+    'validation': [['13-5-2013', '20-5-2013'], ['13-7-2013', '20-7-2013'], ['13-4-2013', '20-4-2013'],
+                   ['7-6-2014', '13-7-2014']],
+    'test': ['30-6-2013', '15-7-2013']
 }
 
 # windows = {
@@ -34,9 +36,13 @@ for window in windows['train']:
     t.set_window(start=window[0], end=window[1])
     train += [t]
 
-validation = DataSet('../data/ukdale.h5')
-validation.clear_cache()
-validation.set_window(start=windows['validation'][0], end=windows['validation'][1])
+validation = []
+for window in windows['validation']:
+    v = DataSet('../data/ukdale.h5')
+    v.clear_cache()
+    v.set_window(start=window[0], end=window[1])
+    validation += [v]
+
 test = DataSet('../data/ukdale.h5')
 test.clear_cache()
 test.set_window(start=windows['test'][0], end=windows['test'][1])
@@ -45,8 +51,8 @@ train_mainslist = []
 train_meterlist = []
 val_mainslist = []
 val_meterlist = []
-train_buildings = [1,2]
-val_buildings = [4]
+train_buildings = [1,2,4,5]
+val_buildings = [1,2,4,5]
 test_building = 1
 sample_period = 6
 meter_key = 'kettle'
@@ -64,7 +70,8 @@ with open(results_file, "w") as text_file:
     text_file.write('USE SLIDING WINDOW' + '\n')
     for window in windows['train']:
         text_file.write('train window: ({}, {})\n'.format(window[0], window[1]))
-    text_file.write('validation window: ({}, {})\n'.format(windows['validation'][0], windows['validation'][1]))
+    for window in windows['validation']:
+        text_file.write('validation window: ({}, {})\n'.format(window[0], window[1]))
     text_file.write('test window: ({}, {})\n'.format(windows['test'][0], windows['test'][1]))
     text_file.write('train buildings: {}\n'.format(train_buildings))
     text_file.write('validation buildings: {}\n'.format(val_buildings))
@@ -81,8 +88,8 @@ for i, b in enumerate(train_buildings):
     train_mainslist += [train_mains]
     train_meterlist += [train_meter]
 
-for i in val_buildings:
-    val_elec = validation.buildings[i].elec
+for i, b in enumerate(val_buildings):
+    val_elec = validation[i].buildings[b].elec
     val_meter = val_elec.submeters()[meter_key]
     val_mains = val_elec.mains()
 
@@ -98,14 +105,14 @@ val_logfile = os.path.join(results_dir, 'validation.log')
 
 if IMPORT:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate, init=False)
-    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-200epochs.h5"))  # TODO: insert last model name
+    rnn.import_model(os.path.join(results_dir, "UKDALE-RNN-kettle-150epochs.h5"))  # TODO: insert last model name
 else:
     rnn = RNNDisaggregator(train_logfile, val_logfile, learning_rate)
 
 start = time.time()
 print("========== TRAIN ============")
-epochs = 200  # TODO: update according to the last model if IMPORT = True
-for i in range(0):
+epochs = 0  # TODO: update according to the last model if IMPORT = True
+for i in range(15):
     rnn.train_across_buildings(train_mainslist, train_meterlist, val_mainslist, val_meterlist, epochs=10,
                                sample_period=sample_period)
     epochs += 10
